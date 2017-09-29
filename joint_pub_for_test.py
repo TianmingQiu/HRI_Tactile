5d0f4eec43574c9c55ac51f2fa8336bb49ff8a1f
TianmingQiu <qtianming@hotmail.com>
2017-09-29 15:42:07 +0200

    Md file

--- joint_pub_for_test.py
+++ joint_pub_for_test.py
@@ -13,17 +13,34 @@ motion = ALProxy("ALMotion", nao_ip, port)
 
 def pubState():
     pub = rospy.Publisher('get_state', String, queue_size = 10)
+    #pub = rospy.Publisher('get_joint', String, queue_size = 10)
     rospy.init_node('state_pub', anonymous=False)
     rate = rospy.Rate(10) # 10hz
-    
+    '''pub = rospy.Publisher('get_joint', Float32MultiArray, queue_size = 1)
+    rospy.init_node('joint_pub', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+    mat = Float32MultiArray()
+    mat.layout.dim.append(MultiArrayDimension())
+    mat.layout.dim.append(MultiArrayDimension())
+    mat.layout.dim[0].label = "height"
+    mat.layout.dim[1].label = "width"
+    mat.layout.dim[0].size = 1
+    mat.layout.dim[1].size = 1
+    mat.layout.dim[0].stride = 1.0*1.0
+    mat.layout.dim[1].stride = 1.0
+    mat.layout.data_offset = 0
+    mat.data = [0]'''
 
     # get joint value method: http://doc.aldebaran.com/1-14/naoqi/motion/control-joint-api.html#ALMotionProxy::getAngles__AL::ALValueCR.bCR
     # The names of differnt joints: http://doc.aldebaran.com/1-14/naoqi/motion/control-joint.html
     # Also, the name could be "Body", "RArm", "RElbowYaw"
+    #names         = "LShoulderPitch"
+    #useSensors  = True
+    
 
     while not rospy.is_shutdown():
         theta1 = motion.getAngles("RElbowRoll", True)
-        theta2 = motion.getAngles("RElbowYaw", True)
+        #theta2 = motion.getAngles("RElbowYaw",      True)
         #angle = theta1+theta2
         # mat.data[0] = sensorAngles
         # rospy.loginfo(mat.data)
